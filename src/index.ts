799d812d7d200be5e995fe05307f0b4698708925
sanderheieren <sanheieren@gmail.com>
2021-03-22 14:08:49 +0100

    making passthrough methods with 'get' for on, trigger and get

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { User } from './Models/User';
 
 const user = new User({ name: 'new record', age: 0 });
-user.save();
+
+// returns the on function from the Eventing class
+// which is what we invoke
+user.on('change', () => {
+	console.log('user was changed');
+});
